984204d9c3b4802d99e5db005295497b6dc1a3fa
yutakaba <lntdk1121@gmail.com>
2025-12-12 23:25:39 +0900

    fix

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import withLinaria from 'next-with-linaria';
-
-/** @type {import('next').NextConfig} */
-const config = {
-  reactStrictMode: true,
-  swcMinify: true,
-};
-
-export default withLinaria(config);
